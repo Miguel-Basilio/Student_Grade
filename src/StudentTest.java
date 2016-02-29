2fbd747d52bf1e5084137c79b190fdfa0ba4ceab
sac-cs112-basilio-miguel <lokoangel06@gmail.com>
2016-02-29 07:25:08 -0800

    program complete and fixed.

---
+++ src/StudentTest.java
@@ -0,0 +1,16 @@
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+
+			Student account1 = new Student ("jane green", 93.5);
+			Student account2 = new Student ("jonh blue", 72.75);
+			
+			System.out .printf("%s`s letter grade is: %s%n",
+					account1.getName(), account1.getLetterGrade());
+			System.out.printf("%s`s letter grade: %s%n", 
+					account2.getName(),account2.getLetterGrade());
+			
+	}
+
+}
